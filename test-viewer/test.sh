ba25db40e62474782b10f80b5fb89d087ad07f3e
Stephan <stephan.soller@helionweb.de>
2014-06-06 15:39:32 +0200

    Cleaned up the streaming test shell script.

--- test-viewer/test.sh
+++ test-viewer/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-ffmpeg -re -i "$1" -map 0:0 -map 0:1 -c:v rawvideo -c:a pcm_s16le -f matroska - | tee \
-	>( ffmpeg -v warning -i -                       -quality realtime -minrate 3M -maxrate 3M -b:v 3M -threads 3 -chunked_post 0 http://localhost:1234/1080p.webm ) \
-	>( ffmpeg -v warning -i - -vf scale=w=1280:h=-1 -quality realtime -minrate 1M -maxrate 1M -b:v 1M -threads 3 -chunked_post 0 http://localhost:1234/720p.webm ) \
-	|  ffmpeg -v warning -i - -vf scale=w=1024:h=-1 -quality realtime -minrate 300K -maxrate 300K -b:v 300K -threads 3 -chunked_post 0 http://localhost:1234/sd.webm
+FFMPEG_OPTS="-v warning -i -"
+ENCODER_OPTS="-quality realtime -threads 3"
+OUTPUT_OPTS="-f webm -chunked_post 0"
+ffmpeg -re -noaccurate_seek -ss 0:00 -i "$1" -map 0:0 -map 0:1 -c:v rawvideo -c:a pcm_s16le -f matroska - | tee \
+	>( ffmpeg $FFMPEG_OPTS                       $ENCODER_OPTS -minrate 3M -maxrate 3M -b:v 3M       $OUTPUT_OPTS http://localhost:1234/test.1080p.webm ) \
+	>( ffmpeg $FFMPEG_OPTS -vf scale=w=1280:h=-1 $ENCODER_OPTS -minrate 1M -maxrate 1M -b:v 1M       $OUTPUT_OPTS http://localhost:1234/test.720p.webm ) \
+	|  ffmpeg $FFMPEG_OPTS -vf scale=w=1024:h=-1 $ENCODER_OPTS -minrate 300K -maxrate 300K -b:v 300K $OUTPUT_OPTS http://localhost:1234/test.sd.webm
